47d0af55319d42e4f0d7e275551be41af7f37622
™ധู้ざຣ้ণს✚ປิʨℓℓҨබମ™ <39564544+0954517662@users.noreply.github.com>
2018-09-19 08:57:58 +0700

    Update config.py

--- linepy/config.py
+++ linepy/config.py
@@ -30,7 +30,7 @@ class Config(object):
     APP_TYPE    = ApplicationType._VALUES_TO_NAMES[368]
     APP_VER     = '2.1.5'
     CARRIER     = '51089, 1-0'
-    SYSTEM_NAME = 'ARIFISTIFIK'
+    SYSTEM_NAME = 'SELFBOT-BY:MAX'
     SYSTEM_VER  = '11.2.5'
     IP_ADDR     = '8.8.8.8'
     EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
